483d05fd1563b918400d95368f92570ae1111e14
SaulAugustoGasca115 <sgasca@cocoschool.mx>
2021-09-16 13:33:58 -0500

    Animation Controller added, Follow Camera added and Clock to move the character added

--- ArtorasProject/Assets/Scripts/Move.cs
+++ ArtorasProject/Assets/Scripts/Move.cs
@@ -20,13 +20,39 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButton(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            MoveToCursor();
         }
 
-        Debug.DrawLine(lastRay.origin,lastRay.direction * 100);
+        UpdateAnimator();
+        
+    }
 
-        GetComponent<NavMeshAgent>().destination = target.position;
+    void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHit  = Physics.Raycast(ray,out hit);
+
+        if(hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+
+        //lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Debug.DrawLine(lastRay.origin,lastRay.direction * 100);
+
+        //GetComponent<NavMeshAgent>().destination = target.position;
+    }
+
+    void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+
+        GetComponent<Animator>().SetFloat("forwardSpeed",speed);
     }
 }
